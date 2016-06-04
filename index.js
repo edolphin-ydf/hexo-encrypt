31c29e5a9120406eaad73d0c7e9084e0f0ef0ce8
edolphin-ydf <shisanbihua@126.com>
2016-06-04 02:13:03 -0700

    refact the struct of code

--- index.js
+++ index.js
@@ -67,186 +67,58 @@
 var CryptoJS = require("crypto-js");
 var hexofs = require('hexo-fs');
 var fs = require('fs');
-var qiniu = require("qiniu");
 var log = require('hexo-log')({
   debug: false,
   silent: false
 });
 
-function getPassword(data) {
-	var pwdfilePath = hexo.base_dir + hexo.config.encrypt.pwdfile;
-	var password = 'default_password';
-	if (fs.existsSync(pwdfilePath)) {
-		password = fs.readFileSync(pwdfilePath).toString();
-		password = password.replace(/\n/g, '');
-	}
-	if (password == 'default_password')
-		password = hexo.config.encrypt.password.toString();
-
-	// if the pwd is setted in post header, then use it
-	if (data.enc_pwd != undefined) {
-		password = data.enc_pwd.toString();
-	}
-	log.debug("password:" + password);
-	return password;
-}
-
-function genQiniuUrl(data, url) {
-	if (hexo.config.encrypt.qiniu_ak == undefined || hexo.config.encrypt.qiniu_ak == undefined) {
-		log.warn("the qiniu ak or sk is incorrect, please check you _config.yml.\n" +
-				"maybe you want this:\n" +
-"encrypt:\n" +
-  "\tqiniu_ak: your access key of qiniu\n" +
-  "\tqiniu_sk: your secret key of qiniu\n");
-		return url;
-	}
-
-	qiniu.conf.ACCESS_KEY = hexo.config.encrypt.qiniu_ak.toString();
-	qiniu.conf.SECRET_KEY = hexo.config.encrypt.qiniu_sk.toString();
-	var ttl = 3600;
-	if (hexo.config.encrypt.img_url_ttl != undefined) {
-		ttl = hexo.config.encrypt.img_url_ttl;
-	}
+var ConfGlobal = require('./lib/conf_global')(hexo.config.encrypt);
+var ConfPost = require('./lib/conf_post');
+var ConfUrl = require('./lib/conf_url');
+var imgUrlGenerator = require('./lib/url_gen');
 
 
-	//构建私有空间的链接
-	var policy = new qiniu.rs.GetPolicy(ttl);
-
-	//生成下载链接url
-	var downloadUrl = policy.makeRequest(url);
-
-	//打印下载的url
-	log.debug('downloadUrl:' + downloadUrl);
-
-	return downloadUrl;
-}
-
-function determineFullFilePath(basedir, path) {
-	// if the path is full path, return it
-	if (path.lastIndexOf("/", 0) == 0) {
-		return path;	
-	}
-
-	// replace ~/ to /
-	if (path.lastIndexOf("~/", 0) == 0) {
-		return process.env.HOME + path.substr(1, path.length);
-	}
-
-	// replace ./ to /
-	var res = path;
-	if (path.lastIndexOf("./", 0) == 0) {
-		res = basedir + path.substring(2, path.length);
-	} else {
-		res = basedir + path;	
-	}
-	return res;
-}
-
-function getFileExt(path) {
-	var dotPos = path.lastIndexOf(".");
-	return path.substr(dotPos+1, path.length);
-}
-
-function genBase64ImgUrl(data, path) {
-	var basedir = data.asset_dir;
-	var fullpath = determineFullFilePath(basedir, path);
-	var ext = getFileExt(fullpath);
-	if (fs.existsSync(fullpath)) {
-		var base64Img = fs.readFileSync(fullpath, "base64");
-		return "data:image/"+ ext + ";base64," + base64Img;
-	} else {
-		log.warn("file " + fullpath + " not found");
-	}
-	return path;
-}
-
-function genUrl(data, url, type) {
-	if (type == 'base64') {
-		return genBase64ImgUrl(data, url);
-	} else if (type == 'qiniu') {
-		return genQiniuUrl(data, url);
-	} else {
-		return url;
-	}
-}
-
-function replaceImgUrl(data) {
-	var baseUrl = undefined;
-	if (hexo.config.encrypt.base_url != undefined) {
-		baseUrl = hexo.config.encrypt.base_url.toString();
-	}
-
-	// type for encode url
-	var globalType = 'qiniu';	// default is qiniu
-	if (hexo.config.encrypt.type != undefined) {
-		globalType = hexo.config.encrypt.type.toString();	
-	}
-
-	if (data.enc_img_type != undefined) {
-		globalType = data.enc_img_type.toString();
-	}
-	log.debug("globalType:" + globalType);
+function replaceImgUrl(conf, data) {
+	var baseUrl = conf.base_url;
 
 	data.content = data.content.replace(/<img [^>]*src=['"]([^'"]+)[^>]*>/gi,
-      function (match, url) {
-		  var tmpurl = url;
+	function (match, url) {
+		var tmpurl = url;
 
-		  var type = globalType;
-		  var qiniuProtocal = "qiniu://";
-		  var base64Protocal = "base64://";
-		  if (url.lastIndexOf(qiniuProtocal, 0) == 0) {
-		  	  tmpurl = tmpurl.substr(url.lastIndexOf(qiniuProtocal, 0) + qiniuProtocal.length, tmpurl.length);
-			  type = "qiniu";
-		  } else if (url.lastIndexOf(base64Protocal, 0) == 0) {
-		 	  tmpurl = tmpurl.substr(url.lastIndexOf(base64Protocal, 0) +  base64Protocal.length, tmpurl.length);
-			  type = "base64";
-		  }
+		var urlConf = ConfUrl.create(conf, url);
 
-		  // the base url is configed and the current url is not a full url and
-		  // the encode img url type is qiniu,  then contact the both
-		  if (baseUrl != undefined
-				  && tmpurl.lastIndexOf("http://", 0) != 0	
-				  && type == 'qiniu') {
-			  tmpurl = baseUrl + tmpurl;	
-		  }
-		  log.debug("\n\turl:" + url + "\n\ttmpurl:" + tmpurl + "\n\ttype:" + type);
-		  return match.replace(url, genUrl(data, tmpurl, type));
-	  });
+		return match.replace(url, imgUrlGenerator.genUrl(data, urlConf));
+	});
+	log.debug("post pic url replaced");
+}
+
+function encrypt(conf, data) {
+	var password = conf.password;
+	var ciphertext = CryptoJS.AES.encrypt(data.content, password);
+	var txt = ciphertext.toString();
+	data.content = '<div id="enc_content">' + txt + '</div>';
+	data.content = data.content + '<script src="/js/crypto-js.js"></script>';
+	data.content = data.content + '<script>'+
+		'var pwd = window.prompt("please input password","");' + 
+		'var txt = document.getElementById("enc_content").innerHTML;' +
+		'var bytes  = CryptoJS.AES.decrypt(txt, pwd);' +
+		'var plaintext = bytes.toString(CryptoJS.enc.Utf8);' +
+		'document.getElementById("enc_content").innerHTML = plaintext' +
+		'</script>';
+	log.info(data.title + " encryped");
 }
 
 hexo.extend.filter.register('after_post_render', function(data){
 
-	var replace = false;
-	if (hexo.config.encrypt.replace_all_url == true) {
-		replace = true;	
-		log.debug("global replace, true");
-	}
-
-	// if the replace rule was set in the post, then use it
-	if (data.enc_replace_url != undefined) {
-		replace = data.enc_replace_url == true? true: false;
-		log.debug("post replace, " + replace);
-	}
-	if (replace) {
-		replaceImgUrl(data);
-		log.debug("post pic url replaced");
+	var confPost = ConfPost.create(ConfGlobal, data);
+	log.debug("replace:" + confPost.replace_img_url);
+	if (confPost.replace_img_url == true) {
+		replaceImgUrl(confPost, data);
 	}
 
 	// encrypt the content
-	if (data.encrypt == true) {
-		var password = getPassword(data);
-		var ciphertext = CryptoJS.AES.encrypt(data.content, password);
-		var txt = ciphertext.toString();
-		data.content = '<div id="enc_content">' + txt + '</div>';
-		data.content = data.content + '<script src="/js/crypto-js.js"></script>';
-		data.content = data.content + '<script>'+
-			'var pwd = window.prompt("please input password","");' + 
-			'var txt = document.getElementById("enc_content").innerHTML;' +
-			'var bytes  = CryptoJS.AES.decrypt(txt, pwd);' +
-			'var plaintext = bytes.toString(CryptoJS.enc.Utf8);' +
-			'document.getElementById("enc_content").innerHTML = plaintext' +
-			'</script>';
-		log.info(data.title + " encryped");
+	if (confPost.encrypt == true) {
+		encrypt(confPost, data);
 	} 
 	return data;
 });
@@ -258,6 +130,5 @@ hexo.extend.filter.register('after_generate', function(){
 	hexofs.copyFile(hexo.base_dir + 'node_modules/hexo-encrypt/crypto-js.js', 
 			hexo.base_dir + 'public/js/crypto-js.js');
 });
-
 
 
